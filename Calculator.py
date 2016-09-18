2f8a0f1a3bedbdc51132d3b9a303f090f9371663
Janny <187231153@qq.com>
2016-09-18 18:00:55 +0800

    Version 3

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,16 @@
 from tkinter import*
 
 # 创建横条型框架
-def frame(root, side):
-    w = Frame(root,borderwidth=4, bd=4, bg="powder blue")
-    w.pack(side=side, expand=YES, fill=BOTH)
-    return w
+def iFrame(root, side):
+    storeObj = Frame(root,borderwidth=4, bd=4, bg="powder blue")
+    storeObj.pack(side=side, expand=YES, fill=BOTH)
+    return storeObj
 
 # 创建按钮
 def button(root, side, text, command=None):
-    w = Button(root, text=text, command=command)
-    w.pack(side=side, expand=YES, fill=BOTH)
-    return w
+    storeObj = Button(root, text=text, command=command)
+    storeObj.pack(side=side, expand=YES, fill=BOTH)
+    return storeObj
 
 
 # 继承了Frame类，初始化程序界面的布局
@@ -24,31 +24,31 @@ class Calculator(Frame):
         display = StringVar()
         # 添加输入框
         Entry(self, relief=RIDGE,
-              textvariable=display,justify='right', bd=30, bg="Powder blue").pack(side=TOP, expand=YES,
+              textvariable=display,justify='right', bd=15, bg="Powder blue").pack(side=TOP, expand=YES,
                                          fill=BOTH)
         # 添加清除按钮
         #clearF = frame(self, TOP)
         #button(clearF, LEFT, 'clear', lambda w=display: w.set(''))
 
         for clearBut in(["CE"],["C"]):
-            keyF = frame(self, TOP)
+            keyF = iFrame(self, TOP)
             for char in clearBut:
-                button(keyF, LEFT, char, lambda w=display, c=char: w.set(''))
+                button(keyF, LEFT, char, lambda storeObj=display, c=char: storeObj.set(''))
 
         # 添加横条型框架以及里面的按钮
         for key in ('123*', '456/', '789-', '0.+'):
-            keyF = frame(self, TOP)
+            keyF = iFrame(self, TOP)
             for char in key:
-                button(keyF, LEFT, char, lambda w=display, c=char: w.set(w.get() + c))
+                button(keyF, LEFT, char, lambda storeObj=display, c=char: storeObj.set(storeObj.get() + c))
 
         # 添加操作符按钮
-        opsF = frame(self, TOP)
+        opsF = iFrame(self, TOP)
         for char in '=':
             if char == '=':
                 btn = button(opsF, LEFT, char)
-                btn.bind('<ButtonRelease - 1>', lambda e, s=self, w=display: s.calc(w), '+')
+                btn.bind('<ButtonRelease - 1>', lambda e, s=self, storeObj=display: s.calc(storeObj), '+')
             else:
-                btn = button(opsF, LEFT, char, lambda w=display, s='%s' % char: w.set(w.get() + s))
+                btn = button(opsF, LEFT, char, lambda storeObj=display, s='%s' % char: storeObj.set(storeObj.get() + s))
 
         # 调用eval函数计算表达式的值
     def calc(self, display):
